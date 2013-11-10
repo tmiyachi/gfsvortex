b6f3aa280ece43b0ea646656d085c9b8549be1b1
tmiyachi <t.miyachi0630@gmail.com>
2013-11-10 16:27:55 +0900

    some changes

--- exp/vortex_move.sh
+++ exp/vortex_move.sh
@@ -21,21 +21,28 @@ BESTTRACKDIR=/mnt/drobo1/Public/cxmldata/track/besttrack
 TCNAME=17PARMA
 IDATE=2009093012
 
-JCAP=190
-MN=p01
-EXP=alera2
+JCAP=${JCAP:-190}
+MN=${MN:-}
+EXP=ncep
+OEXP=r${EXP}
 
 TSTMP="${IDATE:0:4}-${IDATE:4:2}-${IDATE:6:2} ${IDATE:8:2}:00:00"
 clon_obs=`cat ${BESTTRACKDIR}/${IDATE:0:4}/${TCNAME}.tsv | grep "${TSTMP}" | cut -f3`
 clat_obs=`cat ${BESTTRACKDIR}/${IDATE:0:4}/${TCNAME}.tsv | grep "${TSTMP}" | cut -f4`
-clon_new=-9999
-clat_new=-9999
+#clon_new=`echo "scale=2; ${clon_obs}+2" | bc`
+#clat_new=`echo "scale=2; ${clat_obs}+2" | bc`
 
 EXP=${EXP:-ncep}
 MN=${MN:-}
+OEXP=${OEXP:-r${EXP}}
 
-COMIN=${COMIN:-$HOME/gfsinit/${EXP}/${IDATE}/siganl${MN}.${IDATE}.t${JCAP}}
-COMOUT=${COMOUT:-$TOPDIR/output/r${EXP}/${IDATE}/siganl${MN}.${IDATE}.t${JCAP}}
+if [ $JCAP = 382 -a $EXP = 'ncep' ]; then
+    COMIN=${COMIN:-$HOME/gfsinit/${EXP}/${IDATE}/siganl.${IDATE}.t${JCAP}}
+else
+    COMIN=${COMIN:-/mnt/drobo1/Public/gfsinit_cfsr/${IDATE:0:6}/siganl${MN}.${IDATE}.t${JCAP}}
+fi
+
+COMOUT=${COMOUT:-$TOPDIR/gfsinit/${OEXP}/${IDATE}/siganl${MN}.${IDATE}.t${JCAP}}
 DATA=/tmp/$(whoami)/vortex_move
 PGM=$TOPDIR/exec/vortex_move
 
@@ -62,7 +69,7 @@ cat <<EOF > vortex_move_namelist
   clon_obs=${clon_obs},clat_obs=${clat_obs} 
  /
  &param
-  clon_new=${clon_new},clat_new=${clat_new}
+  clon_new=${clon_new:--9999},clat_new=${clat_new:--9999}
  /
  &end
 EOF
@@ -72,4 +79,4 @@ $PGM
 if [ $? -ne 0 ]; then
     exit 99
 fi
-#rm -r -f $DATA
+rm -r -f $DATA
